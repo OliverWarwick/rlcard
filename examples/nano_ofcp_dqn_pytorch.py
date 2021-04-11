a81c957dc1c26a6057008cf80be5e5863f589f5c
Oliver Warwick <oliverwarwick6@gmail.com>
2021-04-11 16:40:59 +0100

    pulled into function for colabs

--- examples/nano_ofcp_dqn_pytorch.py
+++ examples/nano_ofcp_dqn_pytorch.py
@@ -9,90 +9,83 @@ from rlcard.agents import RandomAgent
 from rlcard.utils import set_global_seed, tournament
 from rlcard.utils import Logger
 
-# def main(evaluate_every = 100, 
-# evaluate_num = 1000, 
-# episode_num = 10000, 
-# memory_init_size = 1000, 
-# train_every = 1, 
-# log_dir = './experiments/nano_ofcp_dqn_result/',
-# save_dir = 'models/nano_dqn_pytorch'):
+def training_run(evaluate_every = 1000, 
+                evaluate_num = 2500, 
+                episode_num = 20000, 
+                memory_init_size = 1000, 
+                train_every = 1, 
+                log_dir = None,
+                save_dir = None):
 
-# Make environment
-env = rlcard.make('nano_ofcp', config={'seed': 0})
-eval_env = rlcard.make('nano_ofcp', config={'seed': 0})
+    # Make environment
+    env = rlcard.make('nano_ofcp', config={'seed': 0})
+    eval_env = rlcard.make('nano_ofcp', config={'seed': 0})
 
-# # Set the iterations numbers and how frequently we evaluate the performance
-evaluate_every = 1000
-evaluate_num = 2000
-episode_num = 3000
+    # The paths for saving the logs and learning curves
+    if log_dir is None:
+        log_dir = './experiments/nano_ofcp_dqn_result/'
 
-# The intial memory size
-memory_init_size = 1000
+    # Set up the model saving folder.
+    best_score = 0
+    if save_dir is None:
+        save_dir = 'models/nano_dqn_pytorch'
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
 
-# Train the agent every X steps
-train_every = 1
+    # Set a global seed
+    set_global_seed(0)
 
-# The paths for saving the logs and learning curves
-log_dir = './experiments/nano_ofcp_dqn_result/'
+    agent = DQNAgent(scope='dqn',
+                    action_num=env.action_num,
+                    replay_memory_init_size=memory_init_size,
+                    train_every=train_every,
+                    state_shape=env.state_shape,
+                    mlp_layers=[64, 64],
+                    device=torch.device('cpu'),
+                    epsilon_decay_steps=50000)
+    random_agent = RandomAgent(action_num=eval_env.action_num)
+    env.set_agents([agent, random_agent])
+    eval_env.set_agents([agent, random_agent])
 
-# Set up the model saving folder.
-best_score = 0
-save_dir = 'models/nano_dqn_pytorch'
-if not os.path.exists(save_dir):
-    os.makedirs(save_dir)
+    # Init a Logger to plot the learning curve
+    logger = Logger(log_dir)
 
-# Set a global seed
-set_global_seed(0)
+    # Display infomation about the agents networks.
+    # print("Agents network shape: {}".format(agent.q_estimator.qnet))
+    # print("Agents network layers: {}".format(agent.q_estimator.qnet.fc_layers))
 
-agent = DQNAgent(scope='dqn',
-                 action_num=env.action_num,
-                 replay_memory_init_size=memory_init_size,
-                 train_every=train_every,
-                 state_shape=env.state_shape,
-                 mlp_layers=[64, 64],
-                 device=torch.device('cpu'),
-                 epsilon_decay_steps=50000)
-random_agent = RandomAgent(action_num=eval_env.action_num)
-env.set_agents([agent, random_agent])
-eval_env.set_agents([agent, random_agent])
+    for episode in range(episode_num):
 
-# Init a Logger to plot the learning curve
-logger = Logger(log_dir)
+        # Generate data from the environment
+        trajectories, _ = env.run(is_training=True)
 
-# Display infomation about the agents networks.
-# print("Agents network shape: {}".format(agent.q_estimator.qnet))
-# print("Agents network layers: {}".format(agent.q_estimator.qnet.fc_layers))
+        # Feed transitions into agent memory, and train the agent
+        for ts in trajectories[0]:
+            agent.feed(ts)
 
-for episode in range(episode_num):
+        # Evaluate the performance. Play with random agents.
+        if episode % evaluate_every == 0:
+            
+            tour_score = tournament(eval_env, evaluate_num)[0]
+            if tour_score > best_score:
+                state_dict = agent.get_state_dict()
+                torch.save(state_dict, os.path.join(save_dir, 'best_model.pth'))
+                best_score = tour_score
+                logger.log(str(env.timestep) + "  Saving best model. Accuracy: " + str(best_score))
 
-    # Generate data from the environment
-    trajectories, _ = env.run(is_training=True)
+            logger.log_performance(env.timestep, tournament(eval_env, evaluate_num)[0])
 
-    # Feed transitions into agent memory, and train the agent
-    for ts in trajectories[0]:
-        agent.feed(ts)
+    # Close files in the logger
+    logger.close_files()
 
-    # Evaluate the performance. Play with random agents.
-    if episode % evaluate_every == 0:
-        
-        tour_score = tournament(eval_env, evaluate_num)[0]
-        if tour_score > best_score:
-            state_dict = agent.get_state_dict()
-            torch.save(state_dict, os.path.join(save_dir, 'best_model.pth'))
-            best_score = tour_score
-            logger.log(str(env.timestep) + "  Saving best model. Accuracy: " + str(best_score))
+    # Plot the learning curve
+    logger.plot('DQN')
 
-        logger.log_performance(env.timestep, tournament(eval_env, evaluate_num)[0])
+    # Save model
 
-# Close files in the logger
-logger.close_files()
+    state_dict = agent.get_state_dict()
+    print(state_dict.keys())
+    torch.save(state_dict, os.path.join(save_dir, 'model.pth'))
 
-# Plot the learning curve
-logger.plot('DQN')
-
-# Save model
-
-state_dict = agent.get_state_dict()
-print(state_dict.keys())
-torch.save(state_dict, os.path.join(save_dir, 'model.pth'))
-
+if __name__ == '__main__':
+    training_run()
